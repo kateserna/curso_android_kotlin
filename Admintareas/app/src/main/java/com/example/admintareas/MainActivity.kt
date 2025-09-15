cd13b27f267718e715866e97338f5cd975234bf8
kateserna <kserna.g@gmail.com>
2025-09-15 17:31:26 -0500

    add of resources, image, and strings.

--- Admintareas/app/src/main/java/com/example/admintareas/MainActivity.kt
+++ Admintareas/app/src/main/java/com/example/admintareas/MainActivity.kt
@@ -4,13 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.admintareas.ui.theme.AdminTareasTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,10 +30,13 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             AdminTareasTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    TaskCard(
+                        stringResource(R.string.tasks_completed),
+                        stringResource(R.string.nice_work)
                     )
                 }
             }
@@ -31,17 +45,53 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun TaskText(message: String, congratulation: String, modifier: Modifier = Modifier) {
+    Column(
+        verticalArrangement = Arrangement.Center,
         modifier = modifier
-    )
+    ) {
+        Text(
+            text = message,
+            fontWeight = FontWeight.Bold,
+            textAlign = TextAlign.Center,
+            modifier = Modifier.padding(top = 24.dp, bottom = 8.dp)
+        )
+        Text(
+            text = congratulation,
+            textAlign = TextAlign.Center,
+            fontSize = 16.sp,
+            modifier = Modifier.align(alignment = Alignment.CenterHorizontally),
+        )
+    }
+}
+
+@Composable
+fun TaskCard(message: String, congratulation: String, modifier: Modifier = Modifier){
+    val image = painterResource(R.drawable.ic_task_completed)
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Image(
+            painter = image,
+            contentDescription = null
+        )
+        TaskText(
+            message = message,
+            congratulation = congratulation,
+
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     AdminTareasTheme {
-        Greeting("Android")
+        TaskCard(
+            message = stringResource(R.string.tasks_completed),
+            congratulation = stringResource(R.string.nice_work)
+        )
     }
 }
