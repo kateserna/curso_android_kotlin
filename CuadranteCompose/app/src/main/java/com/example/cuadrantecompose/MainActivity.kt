879efc046ca9974729d98d514166176889589be1
kateserna <kserna.g@gmail.com>
2025-09-16 10:19:37 -0500

    add infocard

--- CuadranteCompose/app/src/main/java/com/example/cuadrantecompose/MainActivity.kt
+++ CuadranteCompose/app/src/main/java/com/example/cuadrantecompose/MainActivity.kt
@@ -4,13 +4,26 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.cuadrantecompose.ui.theme.CuadranteComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,11 +32,11 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             CuadranteComposeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                    ) {
+
                 }
             }
         }
@@ -31,17 +44,53 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun InfoCard(title: String, message: String, background: Color ,modifier: Modifier = Modifier) {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .fillMaxSize()
+            .background(background)
+            .padding(16.dp)
+    ){
+        Text(
+            text = title,
+            fontWeight = FontWeight.Bold,
+            modifier = modifier.padding(bottom = 16.dp)
+        )
+        Text(
+            text = message,
+            textAlign = TextAlign.Justify,
+        )
+    }
+}
+
+@Composable
+fun QuadrantApp() {
+    Column(modifier = Modifier.fillMaxWidth()) {
+        Row(
+            modifier = Modifier.weight(1f)
+        ) {
+            InfoCard(
+                title = stringResource(R.string.title1),
+                message = stringResource(R.string.message1),
+                background = Color(0xFFEADDFF),
+                modifier = Modifier.weight(1f)
+            )
+            InfoCard(
+                title = stringResource(R.string.title2),
+                message = stringResource(R.string.message2),
+                background = Color(0xFFD0BCFF),
+                modifier = Modifier.weight(1f)
+            )
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     CuadranteComposeTheme {
-        Greeting("Android")
+        QuadrantApp()
     }
 }
