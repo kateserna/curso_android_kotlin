b9be890f175438007a8e41b51796f478e34a890a
kateserna <kserna.g@gmail.com>
2025-09-17 15:02:00 -0500

    add image and namecard

--- BusinessCard/app/src/main/java/com/example/businesscard/MainActivity.kt
+++ BusinessCard/app/src/main/java/com/example/businesscard/MainActivity.kt
@@ -4,26 +4,38 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.businesscard.ui.theme.BusinessCardTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             BusinessCardTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background) {
                 }
             }
         }
@@ -31,17 +43,48 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun NameCard(name: String, profession: String, color: Color, modifier: Modifier = Modifier) {
+
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        val image = painterResource(R.drawable.android_logo)
+        Image(
+            painter = image,
+            contentDescription = "logo Android",
+            //contentScale = ContentScale.Crop,
+            modifier = modifier
+                .clip(RoundedCornerShape(30.dp))
+                .size(100.dp)
+                .background(Color(0xFF000000))
+
+
+        )
+        Text(
+            text = stringResource(R.string.name),
+            modifier = modifier
+
+        )
+        Text(
+            text = stringResource(R.string.profession),
+            fontWeight = FontWeight.Bold,
+            color = Color(0xFF640BB9),
+            modifier = modifier
+        )
+    }
+}
+
+@Composable
+fun InfoContact() {
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     BusinessCardTheme {
-        Greeting("Android")
+        NameCard(stringResource(R.string.name), stringResource(R.string.profession), Color(0xFF3ddc84))
     }
 }
