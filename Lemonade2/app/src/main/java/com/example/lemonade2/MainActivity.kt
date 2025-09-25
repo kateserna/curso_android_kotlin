6825677311b39871903e97894f5d3cfb669235e8
kateserna <kserna.g@gmail.com>
2025-09-25 13:19:03 -0500

    Refactor MainActivity.kt and add Lemonade app resources
    - Added a new `imageAsButton` composable function to display an image that acts as a button.
    - Created `LemonadeApp` composable to host the `imageAsButton`.
    - Added new string resources for different states of the Lemonade app.
    - Added new drawable resources for lemon tree, lemon drink, lemon restart, and lemon squeeze images.

--- Lemonade2/app/src/main/java/com/example/lemonade2/MainActivity.kt
+++ Lemonade2/app/src/main/java/com/example/lemonade2/MainActivity.kt
@@ -4,13 +4,28 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.lemonade2.ui.theme.Lemonade2Theme
 
 class MainActivity : ComponentActivity() {
@@ -19,11 +34,11 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Lemonade2Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    LemonadeApp()
                 }
             }
         }
@@ -31,17 +46,45 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun imageAsButton(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        ElevatedButton(
+            onClick = {/*TODO*/},
+            shape = RoundedCornerShape(16.dp),
+            colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.secondaryContainer),
+        ) {
+            Image(
+                painter = painterResource(R.drawable.lemon_tree),
+                contentDescription = stringResource(R.string.lemon_tree),
+                modifier = Modifier
+                    .padding(16.dp)
+            )
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(
+            text = stringResource(R.string.screen1),
+            fontSize = 18.sp,
+            style = MaterialTheme.typography.bodyLarge
+        )
+    }
+}
+
+@Composable
+fun LemonadeApp() {
+    imageAsButton(modifier = Modifier
+        .fillMaxSize()
+        .wrapContentSize(Alignment.Center)
     )
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun LemonadePreview() {
     Lemonade2Theme {
-        Greeting("Android")
+        LemonadeApp()
     }
 }
