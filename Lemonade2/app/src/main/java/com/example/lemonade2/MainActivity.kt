c508cfaf9dd63df9ea7b8f848806077db121f7e3
kateserna <kserna.g@gmail.com>
2025-09-26 12:09:04 -0500

    Implement state, add top bar, update UI
    - Added `androidx.material3` dependency.
    - Implemented state management for lemonade making process using `currentStep` and `squeezeCount`.
    - Created `ImageChangeWithClick` composable to manage image and text based on the current step.
    - Refactored `ImageAsButton` to be a reusable composable for displaying an image button with text.
    - Added a `CenterAlignedTopAppBar` to the `LemonadeApp`.
    - Updated UI elements for better visual appearance, including borders and colors.

--- Lemonade2/app/src/main/java/com/example/lemonade2/MainActivity.kt
+++ Lemonade2/app/src/main/java/com/example/lemonade2/MainActivity.kt
@@ -5,6 +5,8 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -13,16 +15,25 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -34,51 +45,118 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Lemonade2Theme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    LemonadeApp()
-                }
+                LemonadeApp()
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun imageAsButton(modifier: Modifier = Modifier) {
-    Column(
-        modifier = modifier,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        ElevatedButton(
-            onClick = {/*TODO*/},
-            shape = RoundedCornerShape(16.dp),
-            colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.secondaryContainer),
-        ) {
-            Image(
-                painter = painterResource(R.drawable.lemon_tree),
-                contentDescription = stringResource(R.string.lemon_tree),
-                modifier = Modifier
-                    .padding(16.dp)
+fun LemonadeApp() {
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(
+                title = {
+                    Text(
+                        text = "Lemonade",
+                        fontWeight = FontWeight.Bold
+                    )
+                },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer
+                )
             )
         }
-
-        Spacer(modifier = Modifier.height(16.dp))
-        Text(
-            text = stringResource(R.string.screen1),
-            fontSize = 18.sp,
-            style = MaterialTheme.typography.bodyLarge
-        )
+    ) { innerPadding ->
+        Surface(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding),
+            color = MaterialTheme.colorScheme.background,
+            ) {
+                ImageChangeWithClick(modifier = Modifier )
+        }
     }
 }
 
 @Composable
-fun LemonadeApp() {
-    imageAsButton(modifier = Modifier
-        .fillMaxSize()
-        .wrapContentSize(Alignment.Center)
-    )
+fun ImageChangeWithClick(modifier: Modifier = Modifier) {
+    var currentStep by remember { mutableStateOf(1) }
+    var squeezeCount by remember { mutableStateOf(0) }
+
+    when (currentStep) {
+        1 -> ImageAsButton(
+                imageResourceId = R.drawable.lemon_tree,
+                contentDescriptionResourceId = R.string.lemon_tree,
+                textResourceId = R.string.screen1,
+                imageClick = {
+                    currentStep = 2
+                    squeezeCount = (2..4).random()
+                }
+        )
+        2 -> ImageAsButton(
+                imageResourceId = R.drawable.lemon_squeeze,
+                contentDescriptionResourceId = R.string.lemon,
+                textResourceId = R.string.screen2,
+                imageClick = {
+                    squeezeCount--
+                    if (squeezeCount == 0) {
+                        currentStep = 3
+                    }
+                }
+        )
+        3 -> ImageAsButton(
+                imageResourceId = R.drawable.lemon_drink,
+                contentDescriptionResourceId = R.string.glass_of_lemonade,
+                textResourceId = R.string.screen3,
+                imageClick = { currentStep = 4 }
+        )
+        4 -> ImageAsButton(
+                imageResourceId = R.drawable.lemon_restart,
+                contentDescriptionResourceId = R.string.empty_glass,
+                textResourceId = R.string.screen4,
+                imageClick = { currentStep = 1 }
+        )
+    }
+}
+
+
+@Composable
+fun ImageAsButton(
+    imageResourceId: Int,
+    contentDescriptionResourceId: Int,
+    textResourceId: Int,
+    imageClick: () -> Unit,
+    modifier: Modifier = Modifier,
+){
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = modifier
+            .fillMaxSize()
+            .wrapContentSize(Alignment.Center),
+    ) {
+        ElevatedButton(
+            onClick = imageClick,
+            shape = RoundedCornerShape(16.dp),
+            colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.secondaryContainer),
+            modifier = Modifier.border(2.dp, color = Color(105, 205, 216), shape = RoundedCornerShape(16.dp))
+        ) {
+            Image(
+                painter = painterResource(imageResourceId),
+                contentDescription = stringResource(contentDescriptionResourceId),
+                modifier = Modifier
+                    .padding(16.dp)
+            )
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(
+            text = stringResource(textResourceId),
+            fontSize = 18.sp,
+            style = MaterialTheme.typography.bodyLarge
+        )
+    }
 }
 
 @Preview(showBackground = true)
