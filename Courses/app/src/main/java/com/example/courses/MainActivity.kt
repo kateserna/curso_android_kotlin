99ff6f83f7c8522662e07cf8ee35719980155db2
kateserna <kserna.g@gmail.com>
2025-10-18 23:05:39 -0500

    feat: Courses App Modification
    - Sets up the basic structure for the "Courses" Android project using Jetpack Compose.
    - Implements the main UI in `MainActivity`, which displays a `LazyVerticalGrid` of course topics.
    - Creates a `TopicCard` composable to display an image, name, and the number of courses for each topic.
    - Defines the `Topic` data class to model the course data.
    - Adds a `DataSource` object to provide a static list of course topics.
    - Includes string resources for topic names and dimension resources for padding.
    - Adds all necessary drawable images for the different course topics and an `ic_grain` icon.

--- Courses/app/src/main/java/com/example/courses/MainActivity.kt
+++ Courses/app/src/main/java/com/example/courses/MainActivity.kt
@@ -4,13 +4,38 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.courses.data.DataSource
+import com.example.courses.model.Topic
 import com.example.courses.ui.theme.CoursesTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,10 +44,14 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             CoursesTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                Scaffold(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .statusBarsPadding(),
+                    //color = MaterialTheme.colorScheme.background
+                ) { innerPadding ->
+                    CoursesApp(
+                       modifier = Modifier.padding(innerPadding)
                     )
                 }
             }
@@ -31,17 +60,83 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun CoursesApp(modifier: Modifier = Modifier) {
+    CourseList(
+        courseList = DataSource.topics,
         modifier = modifier
     )
 }
 
+@Composable
+fun CourseList(courseList: List<Topic>,modifier: Modifier = Modifier) {
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(2),
+        modifier = modifier.padding(dimensionResource(R.dimen.padding_small)),
+        verticalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.padding_small)),
+        horizontalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.padding_small))
+    ) {
+        items(items = courseList) { topic ->
+            TopicCard(topic = topic)
+
+        }
+    }
+}
+
+@Composable
+fun TopicCard(topic: Topic, modifier: Modifier = Modifier) {
+    Card(
+        modifier = modifier,
+        colors = CardDefaults.cardColors(containerColor = Color(221, 219, 222, 255)) //establece el cde fondo del card
+    ) {
+        Row{
+            Image(
+                painter = painterResource(id = topic.imageResourceId),
+                contentDescription = stringResource(id = topic.stringResourceId),
+                modifier = Modifier
+                    .size(68.dp) // El ncho y alto de la imagen sera 68
+                    .aspectRatio(1f), // La imagen tendra un aspecto de relacion especifico 1:1 es decir un cuadrado, en este caso no es necesaria esta linea.
+                contentScale = ContentScale.Crop
+            )
+            Column {
+                Text(
+                    text = stringResource(topic.stringResourceId),
+                    style = MaterialTheme.typography.bodyMedium,
+                    color = Color(128, 128, 128, 255),
+                    modifier = Modifier.padding(
+                        start = dimensionResource(R.dimen.padding_medium),
+                        top = dimensionResource(R.dimen.padding_medium),
+                        end = dimensionResource(R.dimen.padding_medium),
+                        bottom = dimensionResource(R.dimen.padding_small)
+                    ),
+
+                )
+                Row(modifier = Modifier.padding(start = dimensionResource(R.dimen.padding_medium)),
+                    verticalAlignment = Alignment.CenterVertically //Alinea los elementos verticalmente al centro
+                    ) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.ic_grain),
+                        contentDescription = null,
+                        tint = Color(128, 128, 128, 255)
+                    )
+                    Text(
+                        text = topic.numberOfCourses.toString(),
+                        style = MaterialTheme.typography.labelMedium,
+                        color = Color(128, 128, 128, 255),
+                        modifier = Modifier.padding(start = dimensionResource(R.dimen.padding_small)),
+
+                    )
+                }
+            }
+
+        }
+
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun CoursesAppPreview() {
     CoursesTheme {
-        Greeting("Android")
+        CoursesApp()
     }
 }
