a5ecb48beeb5984c7e3a792252f2072dfc6e3ea1
kateserna <kserna.g@gmail.com>
2025-10-25 12:20:36 -0500

    feat: Implement hero list and item card composables
    - Creates a new `HeroesScreen.kt` file to display hero information.
    - Implements `HeroListItem`, a composable that uses `LazyColumn` to efficiently display a list of heroes.
    - Implements the `HeroItem` composable, which displays a single hero's details within a Material 3 `Card`.
    - The `HeroItem` card uses a `Row` to structure the content, showing the hero's name and description on the left and their image on the right.
    - Clips the hero's image to `MaterialTheme.shapes.small`.
    - Updates `dimens.xml` by renaming `image_size` to `height_size` and increasing its value to `72dp` to define the card's minimum height.

---
+++ HeroApp/app/src/main/java/com/example/heroapp/HeroesScreen.kt
@@ -0,0 +1,123 @@
+package com.example.heroapp
+
+import android.content.res.Configuration
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.sizeIn
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.heroapp.model.Hero
+import com.example.heroapp.model.HeroesRepository.heroes
+import com.example.heroapp.ui.theme.HeroAppTheme
+
+@Composable
+fun HeroListItem(
+    heroes: List<Hero>,
+    modifier: Modifier = Modifier
+) {
+    LazyColumn {
+        items(items = heroes) { hero ->
+            HeroItem(
+                hero = hero,
+                modifier = Modifier.padding(
+                    vertical = dimensionResource(R.dimen.padding_small),
+                    horizontal = dimensionResource(R.dimen.padding_medium))
+            )
+        }
+    }
+}
+
+@Composable
+fun HeroItem(
+    hero: Hero,
+    modifier: Modifier = Modifier
+) {
+    Card(
+        elevation = CardDefaults.cardElevation(defaultElevation = 2.dp),
+        //shape = MaterialTheme.shapes.medium,
+        modifier = modifier
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(dimensionResource(R.dimen.padding_medium))
+                .sizeIn(minHeight = dimensionResource(R.dimen.height_size)),
+        ) {
+            Column(modifier = Modifier.weight(1f)) {
+                Text(
+                    text = stringResource(hero.nameRes),
+                    style = MaterialTheme.typography.displaySmall
+                )
+                Text(
+                    text = stringResource(hero.descriptionRes),
+                    style = MaterialTheme.typography.bodyLarge
+                )
+            }
+            Spacer(Modifier.width(dimensionResource(R.dimen.padding_medium)))
+            Box( modifier = Modifier
+                .size(dimensionResource(R.dimen.height_size))
+                .clip(MaterialTheme.shapes.small)
+            ) {
+                Image(
+                    painter = painterResource(hero.imageRes),
+                    contentDescription = null,
+                    alignment = Alignment.TopCenter,
+                    contentScale = ContentScale.FillWidth,
+                )
+            }
+        }
+    }
+}
+
+@Preview("Light Theme")
+@Preview("Dark Theme", uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Composable
+fun HeroPreview() {
+    val hero = Hero(
+        R.string.hero1,
+        R.string.description1,
+        R.drawable.android_superhero1
+    )
+    HeroAppTheme {
+        HeroItem(hero = hero)
+    }
+}
+
+@Preview("Heroes List")
+@Composable
+fun HeroesPreview() {
+    HeroAppTheme(darkTheme = false) {
+        Surface (
+            color = MaterialTheme.colorScheme.background
+        ) {
+            /* Important: It is not a good practice to access data source directly from the UI.
+            In later units you will learn how to use ViewModel in such scenarios that takes the
+            data source as a dependency and exposes heroes.
+            */
+           HeroListItem(heroes)
+        }
+    }
+}
+
