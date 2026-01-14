730a573f4ea2ec030e3a7a003dba7d3e2be94dc2
kateserna <kserna.g@gmail.com>
2026-01-14 11:51:34 -0500

    build: Update Gradle and project dependencies
    - Upgrades the Gradle version from 8.12 to 8.13.
    - Updates the Android Gradle Plugin from `8.8.0` to `8.13.2`.
    - Bumps various project dependencies to their newer versions:
      - Compose BOM from `2024.12.01` to `2025.12.01`
      - Lifecycle from `2.8.7` to `2.10.0`
      - Retrofit from `2.9.0` to `3.0.0`
      - OkHttp from `4.11.0` to `5.3.2`
      - Kotlinx Serialization from `1.6.0` to `1.9.0`
      - Kotlinx Coroutines Test from `1.7.1` to `1.10.2`

--- MarsPhotos/app/build.gradle.kts
+++ MarsPhotos/app/build.gradle.kts
@@ -67,25 +67,25 @@ android {
 dependencies {
 
     // Import the Compose BOM
-    implementation(platform("androidx.compose:compose-bom:2024.12.01"))
+    implementation(platform("androidx.compose:compose-bom:2025.12.01"))
     implementation("androidx.activity:activity-compose:1.9.3")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.core:core-ktx:1.15.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.7")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.10.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.10.0")
     // Retrofit
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:retrofit:3.0.0")
     // Kotlin serialization Converter
     implementation("com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:1.0.0")
-    implementation("com.squareup.okhttp3:okhttp:4.11.0")
+    implementation("com.squareup.okhttp3:okhttp:5.3.2")
     // Kotlin serialization
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.9.0")
     // Coil
     implementation("io.coil-kt:coil-compose:2.7.0")
     testImplementation("junit:junit:4.13.2")
-    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.1")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.10.2")
 
     debugImplementation("androidx.compose.ui:ui-test-manifest")
     debugImplementation("androidx.compose.ui:ui-tooling")
